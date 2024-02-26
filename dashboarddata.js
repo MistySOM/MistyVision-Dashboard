ae2c9f6ec68e5f9a5135a50e25eea64bdd0c4c2d
Victor Yu <victor.yu@mistywest.com>
2024-02-26 14:51:22 -0800

    MVP with live data displayed in Dashboard

--- dashboarddata.js
+++ dashboarddata.js
@@ -1,7 +1,7 @@
 let websocket = null;
 
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://cosmosdbmistyvisiontrafficdatatrigger.azurewebsites.net/api/getWebPubSubToken?code=FZtORSBboR2pNdLFU6RAwHmcXistCf1RPJAiNP_FnVAtAzFuM34AiQ==')
+    fetch('https://mistyvisionfunctionapp.azurewebsites.net/api/getWebPubSubToken')
     .then(response => response.text())
     .then(url => {
         websocket = new WebSocket(url);
@@ -14,21 +14,38 @@ document.addEventListener('DOMContentLoaded', function() {
         websocket.onmessage = event => {
             const message = JSON.parse(event.data);
             console.log('Message received:', message);
-            // add code here Victor
+
+            const videoRate = parseInt(message["video_rate"]);
+            const drpaiRate = parseInt(message["drpai_rate"]);
+            const timestamp = message.timestamp;
+            const timePortion = timestamp.split('T')[1].split('.')[0];
+            const trackedPerHour = parseInt(message["tracked_per_hour"]);
+
+            const pstDateTime = new Date(timestamp).toLocaleString('en-US', {
+                    timeZone: 'America/Vancouver',
+                    hour12: false,
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric'
+                });
+
+            document.getElementById("videoRate").innerHTML= videoRate + " fps";
+            document.getElementById("drpaiRate").innerHTML= drpaiRate + " fps";
+            document.getElementById("timePortion").innerHTML= pstDateTime;
+            document.getElementById("trackedRate").innerHTML = trackedPerHour
+
         };
 
-        websocket.onerror = error => {
-            console.error('WebSocket error:', error);
+        websocket.onerror = function(event) {
+            console.error('WebSocket error:', event);
         };
 
-        websocket.onclose = () => {
-            console.log('Connection closed');
+        websocket.onclose = function(event) {
+            console.log('Connection closed', event.reason, 'Code:', event.code);
         };
     })
     .catch(error => console.error('Fetching Web PubSub token failed:', error));
 });
 
-x = 10;
-document.getElementById("videoRate").innerHTML=x + " fps";
 
 console.log()
