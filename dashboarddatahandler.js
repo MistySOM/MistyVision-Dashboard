8be7114e41f367a484d84f5549a44b9140d4098c
Victor Yu <victor.yu@mistywest.com>
2024-03-08 01:50:41 -0800

    Updated MVP dashboard layout

--- dashboarddatahandler.js
+++ dashboarddatahandler.js
@@ -18,10 +18,10 @@ export function handleWebSocketMessage(event) {
             second: 'numeric'
         });
 
-    document.getElementById("videoRate").innerHTML = videoRate + " fps";
-    document.getElementById("drpaiRate").innerHTML = drpaiRate + " fps";
+    document.getElementById("videoRate").innerHTML = videoRate + " FPS";
+    document.getElementById("drpaiRate").innerHTML = drpaiRate + " FPS";
     document.getElementById("timePortion").innerHTML = pstDateTime;
-    document.getElementById("trackedRate").innerHTML = trackedPerHour;
+    document.getElementById("trackedRate").innerHTML = trackedPerHour + " OBJECTS / DAY";
 
     } catch (error) {
             console.error('Error parsing WebSocket message:', error);
