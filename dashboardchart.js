b79a878bb242358ff87f5f5b4a7ba69553953279
Victor Yu <victor.yu@mistywest.com>
2024-03-11 19:06:28 -0700

    Updated MVP dashboard

--- dashboardchart.js
+++ dashboardchart.js
@@ -15,20 +15,49 @@ function initializeDashboardChart() {
     };
 
     const options = {
-        cutout: '80%',
+        cutout: '90%',
         legend: {
-            position: 'bottom'
-        }
+            position: 'bottom',
+            labels: {
+                usePointStyle: true, // Use point style for legend
+            },
+        },
+        elements: {
+            arc: {
+                borderRadius: 10,
+                borderWidth: 0,
+            },
+        },
+        plugins: {
+            afterDraw: function (chart) {
+                const ctx = chart.ctx;
+                const canvas = chart.canvas;
 
+                const text = 'Center Text'; // Replace with your desired text
+                const fontSize = 16; // Set your desired font size
+                const fontFamily = 'Arial'; // Set your desired font family
 
+                ctx.font = fontSize + 'px ' + fontFamily;
+                ctx.fillStyle = 'white'; // Set your desired text color
+                ctx.textAlign = 'center';
+                ctx.textBaseline = 'middle';
 
+                ctx.fillText(text, centerX, centerY);
+            }
+        },
+        center: {
+        // Set the center coordinates
+        x: '50%',
+        y: '50%',
+        },
     };
+
 
     const dashboardChart = new Chart(chart, {
         type: 'doughnut',
         data: data,
         options: options
     });
-}
 
+}
 window.addEventListener('load', initializeDashboardChart);
