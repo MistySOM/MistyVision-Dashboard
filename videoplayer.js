41956db2c29b40d0f3116b9d8a50e6efb7babe65
Matin Lotfaliei <matinlotfali@gmail.com>
2024-06-04 18:40:07 -0700

    Update videoplayer.js

--- videoplayer.js
+++ videoplayer.js
@@ -2,7 +2,6 @@ export default class VideoPlayer {
 
     constructor() {
         this.liveVideo = false;
-        this.videojs_player = null;
         this.init();
     }
 
@@ -10,7 +9,7 @@ export default class VideoPlayer {
         fetch("https://mistyvisionfunctionapp.azurewebsites.net/api/getliveeventoutputhls", {redirect:'follow'}).then(response => {
             console.log(response.url);
             const source = { src:response.url, type:'application/x-mpegURL' };
-            this.videojs_player = videojs('hls-video', {
+            const videojs_player = videojs('hls-video', {
                 autoplay: 'muted',
                 liveui: true,
                 inactivityTimeout: 0,
@@ -18,25 +17,24 @@ export default class VideoPlayer {
             }, () => {
                 console.log('Video-js is ready!');
 
-                this.videojs_player.on('timeupdate', this.change);
-                this.videojs_player.on('durationchange', this.change);
-                this.videojs_player.on('play', this.change);
-                this.videojs_player.on('pause', this.change);
+                const change = () => {
+                    if ((videojs_player.liveTracker != null && videojs_player.liveTracker.atLiveEdge()) || videojs_player.liveDisplay != null) {
+                        if (this.liveVideo == false) {
+                            console.log('Video player is live.');
+                        }
+                        this.liveVideo = true;
+                    } else if ((videojs_player.liveTracker != null && videojs_player.liveTracker.behindLiveEdge()) || videojs_player.liveDisplay == null) {
+                        if (this.liveVideo == true) {
+                            console.log('Video player is not live.');
+                        }
+                        this.liveVideo = false;
+                    }
+                }                
+                videojs_player.on('timeupdate', change);
+                videojs_player.on('durationchange', change);
+                videojs_player.on('play', change);
+                videojs_player.on('pause', change);
             });
         });
     };
-
-    change() {
-        if ((this.videojs_player.liveTracker != null && this.videojs_player.liveTracker.atLiveEdge()) || this.videojs_player.liveDisplay != null) {
-            if (this.liveVideo == false) {
-                console.log('Video player is live.');
-            }
-            this.liveVideo = true;
-        } else if ((this.videojs_player.liveTracker != null && this.videojs_player.liveTracker.behindLiveEdge()) || this.videojs_player.liveDisplay == null) {
-            if (this.liveVideo == true) {
-                console.log('Video player is not live.');
-            }
-            this.liveVideo = false;
-        }
-    }
 }
